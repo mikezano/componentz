e950f82d806da8934e27d0f12c417c415a13e7f3
mikezano <mikezano@gmail.com>
2018-07-07 19:14:18 -0700

    working with vue-prism

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-//import '../public/prism.css';
-//import '../public/prism.js';
 import Vue from 'vue';
-import VuePrism from 'vue-prism';
+//import VuePrism from 'vue-prism';
 import App from './App.vue';
 import router from './router/index';
 import store from './store';
@@ -11,12 +9,10 @@ Vue.config.productionTip = false;
 import 'vue-awesome/icons';
 import Icon from 'vue-awesome/components/Icon';
 Vue.component('icon', Icon);
-Vue.use(VuePrism);
-
-// require('../public/prism.css');
+//Vue.use(VuePrism);
 
 new Vue({
-	router,
-	store,
-	render: (h: any) => h(App),
+  router,
+  store,
+  render: (h: any) => h(App),
 }).$mount('#app');
