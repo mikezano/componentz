94690cfb54dd4c1e6b5fee41054c9c9f40bf2b61
Michael A Manzano <michael.a.manzano@intel.com>
2018-08-22 13:18:21 -0700

    borders

--- src/components/StyleGuide/AnimatedBorders/Clockwise.vue
+++ src/components/StyleGuide/AnimatedBorders/Clockwise.vue
@@ -14,70 +14,70 @@ export default class Clockwise extends Vue {}
 
 <style lang="scss" scoped>
 @import '../../../sass/colors';
-// .clockwise {
-// 	height: 6rem;
-// 	width: 10rem;
+$border-width: 20px;
+.clockwise {
+	height: 6rem;
+	width: 10rem;
+	box-sizing: border-box;
 
-// 	box-shadow: inset 0 0 0 0.1rem $bs_red;
-// 	position: relative;
-// 	box-sizing: border-box;
+	box-shadow: inset 0 0 0 $border-width $bs_red;
+	position: relative;
+	vertical-align: middle;
 
-// 	&::before,
-// 	&::after {
-// 		content: '';
-// 		box-sizing: inherit;
-// 		position: absolute;
-// 		border: 0.1rem solid transparent;
-// 		width: 0;
-// 		height: 0;
-// 	}
+	&::before,
+	&::after {
+		box-sizing: inherit;
+		content: '';
+		position: absolute;
+		border: $border-width solid transparent;
+		width: 0;
+		height: 0;
+	}
 
-// 	&::before {
-// 		top: 0;
-// 		left: 0;
-// 	}
+	&::before {
+		top: 0;
+		left: 0;
+	}
 
-// 	&::after {
-// 		bottom: 0;
-// 		right: 0;
-// 	}
+	&::after {
+		bottom: 0;
+		right: 0;
+	}
 
-// 	&:hover {
-// 		cursor: pointer;
-// 	}
+	&:hover {
+		cursor: pointer;
+		color: $bs_blue;
+	}
 
-// 	&:hover {
-// 		&::before {
-// 			width: 100%;
-// 			height: 100%;
-// 			border-top-color: $bs_blue;
-// 			border-right-color: $bs_blue;
-// 			transition: width 0.25s ease-in, height 0.25s ease-out 0.25s;
-// 		}
+	&:hover::before,
+	&:hover::after {
+		width: 100%;
+		height: 100%;
+	}
 
-// 		&::after {
-// 			width: 100%;
-// 			height: 100%;
-// 			border-bottom-color: $bs_blue;
-// 			border-left-color: $bs_blue;
-// 			transition: width 0.25s ease-in 0.5s, height 0.25s ease-out 0.75s;
-// 		}
-// 	}
-// }
+	&:hover::before {
+		border-top-color: $bs_blue;
+		border-right-color: $bs_blue;
+		transition: width 0.25s ease-out, height 0.25s ease-out 0.25s;
+	}
+	&:hover::after {
+		border-bottom-color: $bs_blue;
+		border-left-color: $bs_blue;
+		transition: border-color 0s ease-out 0.5s, width 0.25s ease-out 0.5s,
+			height 0.25s ease-out 0.75s;
+	}
+}
+
 button {
 	background: none;
 	border: 0;
 	box-sizing: border-box;
 	margin: 1em;
 	padding: 1em 2em;
-
-	// Using inset box-shadow instead of border for sizing simplicity
-	box-shadow: inset 0 0 0 2px red;
+	box-shadow: inset 0 0 0 4px red;
 	color: red;
 	font-size: inherit;
 	font-weight: 700;
-
-	// Required, since we're setting absolute on pseudo-elements
 	position: relative;
 	vertical-align: middle;
 
@@ -96,7 +96,7 @@ button {
 	&::before,
 	&::after {
 		// Set border to invisible, so we don't see a 4px border on a 0x0 element before the transition starts
-		border: 2px solid transparent;
+		border: 4px solid transparent;
 		width: 0;
 		height: 0;
 	}
