711e716737a87b8a5a80e168622fdcdd6e7b13c0
Michael A Manzano <michael.a.manzano@intel.com>
2019-01-24 16:48:33 -0700

    square box improvements

--- src/components/StyleGuide/Checkboxes/SquareBox.vue
+++ src/components/StyleGuide/Checkboxes/SquareBox.vue
@@ -17,42 +17,47 @@ export default class SquareBox extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-.checkbox {
-	&__label {
-		position: relative;
-		cursor: pointer;
-		padding: 0;
+@mixin checkboxes($scale: 1) {
+	.checkbox {
+		transform: scale($scale);
+		&__label {
+			position: relative;
+			cursor: pointer;
+			padding: 0;
+		}
+		&__label:before {
+			content: "";
+			display: inline-block;
+			width: 1rem;
+			height: 1rem;
+			background-color: white;
+			margin-right: 0.2rem;
+		}
+		&__button {
+			opacity: 0;
+		}
+		&__button:hover + .checkbox__label:before {
+			background-color: whitesmoke;
+		}
+		&__button:focus + .checkbox__label:before {
+			box-shadow: 0 0 0 0.1rem rgba(0, 0, 0, 0.12);
+		}
+		&__button:checked + .checkbox__label:after {
+			content: "";
+			position: absolute;
+			left: 0.25rem;
+			top: 0.6rem;
+			background: black;
+			width: 0.1rem;
+			height: 0.1rem;
+			box-shadow: 0.1rem 0 0 black, 0.2rem 0 0 black,
+				0.2rem -0.1rem 0 black, 0.2rem -0.2rem 0 black,
+				0.2rem -0.3rem 0 black, 0.2rem -0.4rem 0 black;
+			transform: rotate(45deg);
+		}
 	}
-	&__label:before {
-		content: "";
-		display: inline-block;
-		//vertical-align: text-top;
-		width: 1rem;
-		height: 1rem;
-		background-color: white;
-		margin-right: 0.2rem;
-	}
-	&__button {
-		opacity: 0;
-	}
-	&__button:hover + &__label:before {
-		background-color: whitesmoke;
-	}
-	&__button:focus + &__label:before {
-		box-shadow: 0 0 0 0.1rem rgba(0, 0, 0, 0.12);
-	}
-	&__button:checked + &__label:after {
-		content: "";
-		position: absolute;
-		left: 0.25rem;
-		top: 0.6rem;
-		background: black;
-		width: 0.1rem;
-		height: 0.1rem;
-		box-shadow: 0.1rem 0 0 black, 0.2rem 0 0 black, 0.2rem -0.1rem 0 black,
-			0.2rem -0.2rem 0 black, 0.2rem -0.3rem 0 black,
-			0.2rem -0.4rem 0 black;
-		transform: rotate(45deg);
-	}
+} //checkboxes end
+.container {
+	@include checkboxes();
 }
 </style>
