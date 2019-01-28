c519802adb212928acf495bf949fbbc8dd62445f
mikezano <mikezano@gmail.com>
2019-01-27 22:57:15 -0700

    BEM tabs

--- src/components/StyleGuide/Tabs/Underlined.vue
+++ src/components/StyleGuide/Tabs/Underlined.vue
@@ -1,22 +1,23 @@
 <template lang="pug">
 .container
-	input#tab1(type="radio" name="tabs" checked)
-	label(for="tab1") Tab 1
-	input#tab2(type="radio" name="tabs")
-	label(for="tab2") Tab 2
-	input#tab3(type="radio" name="tabs")
-	label(for="tab3") Tab 3
-	input#tab4(type="radio" name="tabs")
-	label(for="tab4") Tab 4
-	.content
-		.content1
-			| Content for Tab 1
-		.content2
-			| Content for Tab 2
-		.content3
-			img(src="https://tinyurl.com/y8n2spmr" width="200" height="200")
-		.content4
-			| Content for Tab 4
+	.tabs
+		input.tabs__tab#tab1(type="radio" name="tabs" checked)
+		label.tabs__label(for="tab1") Tab 1
+		input.tabs__tab#tab2(type="radio" name="tabs")
+		label.tabs__label(for="tab2") Tab 2
+		input.tabs__tab#tab3(type="radio" name="tabs")
+		label.tabs__label(for="tab3") Tab 3
+		input.tabs__tab#tab4(type="radio" name="tabs")
+		label.tabs__label(for="tab4") Tab 4
+		.tabs__content
+			.content1
+				| Content for Tab 1
+			.content2
+				| Content for Tab 2
+			.content3
+				img(src="https://tinyurl.com/y8n2spmr" width="200" height="200")
+			.content4
+				| Content for Tab 4
 </template>
 
 
@@ -29,38 +30,44 @@ export default class Underlined extends Vue {}
 <style lang="scss" scoped>
 @import '../../../sass/colors';
 
-@mixin underlined_tabs() {
-	.content > div {
-		display: none;
-		font-size: 14px;
-		padding: 10px;
-		height: 300px;
-		border-bottom: 1px solid lightgray;
-	}
-	input {
-		display: none;
-	}
-	label {
-		display: inline-block;
-		padding: 5px 10px;
-		text-align: center;
-		margin: 0 2px;
-		border-bottom: 4px solid #bebebe;
-		transition: all 0.2s ease-out;
-	}
-	label:hover {
-		cursor: pointer;
-		border-bottom: 4px solid $vue_green;
-	}
-	input:checked + label {
-		border-bottom: 4px solid $vue_green;
-	}
+.container {
+	//display: flex;
+	flex-direction: row;
+}
 
-	#tab1:checked ~ .content .content1,
-	#tab2:checked ~ .content .content2,
-	#tab3:checked ~ .content .content3,
-	#tab4:checked ~ .content .content4 {
-		display: block;
+@mixin underlined_tabs($tabCount: 2) {
+	.tabs {
+		&__content > div {
+			display: none;
+			font-size: 14px;
+			padding: 10px;
+			height: 300px;
+			border-bottom: 1px solid lightgray;
+		}
+		&__tab {
+			display: none;
+		}
+		&__label {
+			display: inline;
+			padding: 5px 10px;
+			text-align: center;
+			margin: 0 2px;
+			border-bottom: 4px solid #bebebe;
+			transition: all 0.2s ease-out;
+		}
+		&__label:hover {
+			cursor: pointer;
+			border-bottom: 4px solid $vue_green;
+		}
+		&__tab:checked + label {
+			border-bottom: 4px solid $vue_green;
+		}
+
+		@for $i from 1 through $tabCount {
+			#tab#{$i} ~ .content .content#{$i} {
+				display: block;
+			}
+		}
 	}
 } //underlined_tabs end
 
