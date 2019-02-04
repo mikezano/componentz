e6fac21122fb721ba89877b51e1960d0e925a910
Michael A Manzano <michael.a.manzano@intel.com>
2019-02-04 11:17:28 -0700

    Forms for all

--- src/components/StyleGuide/Forms/SimpleForm.vue
+++ src/components/StyleGuide/Forms/SimpleForm.vue
@@ -1,44 +1,45 @@
 <template lang="pug">
-div.example
-	form.form
-		fieldset 
-			legend.form__legend Person Search
-			label.form__label First name
-			input.form__input(
+div
+	form#example.simple-form
+		fieldset.simple-form__fieldset
+			legend.simple-form__legend Person Search
+			label.simple-form__label First name
+			input.simple-form__input(
 				type="text"
 				name="name"
 				placeholder="Enter a first name")
-			label.form__label Last Name
-			input.form__input(
+			label.simple-form__label Last Name
+			input.simple-form__input(
 				type="text"
 				name="name"
 				placeholder="Enter a last name")
-			.form__footer
+			.simple-form__footer
 				button.simple-button(@click='showName') OK
 				button.simple-button No
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-@Component({ name: 'SimpleForm' })
+import { Component, Vue } from "vue-property-decorator";
+@Component({ name: "SimpleForm" })
 export default class SimpleForm extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-@import 'src/mixifier/ads_mixer';
+@import "../../../mixifier/ads_mixer";
 $mainD1: gray;
-@mixin simple-form() {
-	.form {
-		//width: 20rem;
-		fieldset {
+@mixin simple-form($bg-color: #eee) {
+	$s: simple-form;
+	&.#{$s} {
+		.#{$s}__fieldset {
 			padding: 1rem;
 			text-align: left;
+			background-color: $bg-color;
 		}
-		&__legend {
+		.#{$s}__legend {
 			font-weight: bold;
 		}
-		&__input,
-		&__dropdown {
+		.#{$s}__input,
+		.#{$s}__dropdown {
 			display: block;
 			width: 100%;
 			padding: 0.3rem;
@@ -46,18 +47,19 @@ $mainD1: gray;
 			border: 0.1rem solid $mainD1;
 			box-sizing: border-box;
 		}
-		&__label {
+		.#{$s}__label {
 			padding: 0;
 			margin: 0;
 			display: block;
 			font-size: 1rem;
 		}
-		&__checkboxes {
+		.#{$s}__checkboxes {
 			margin: 0.2rem auto 0.6rem;
 			@include checkbox-group(1);
 		}
-		&__footer {
+		.#{$s}__footer {
 			display: flex;
+			align-items: center;
 			button {
 				@include simpleButton();
 			}
@@ -66,8 +68,8 @@ $mainD1: gray;
 			}
 		}
 	}
-} //cricle-btn end
-.example {
+} //simple-form end
+#example {
 	@include simple-form();
 }
 </style>
