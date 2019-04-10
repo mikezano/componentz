075b6a4350c5cdfbc8a7f00a318a669c6287b9cf
mikezano <mikezano@gmail.com>
2019-04-09 22:33:16 -0700

    hashing

--- src/store.ts
+++ src/store.ts
@@ -6,53 +6,60 @@ import { debug } from 'util';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		counter: 2,
-		check: registry,
-		isSingleComponentRoute: false,
-		fromEl: null,
-		toEl: null,
-		isTransitioning: false,
-		scrollPosition: 0,
-		components: [],
-		componentHash: registry.componentHash
-	},
-	getters: {
-		getFiles: (state: any, getters) => () => {
-			return state.componentHash;
-		},
-		getComponent: (state: any, getters) => (name: string) => {
-			const result = state.check.get(name);
-			return result;
-		},
-		getHtmlSingleFile: (state: any, getters) => (name: string) => {
-			const result = state.check.singleFileHash.get(name);
-			return result;
-		},
-		getScrollPosition: (state, getters) => () => {
-			return state.scrollPosition;
-		},
-		getComponents: (state, getters) => () => {
-			return state.components;
-		},
-	},
-	mutations: {
-		setFromEl: (state, el) => {
-			console.log(el, 'SetFromEl store.ts');
-			state.fromEl = el;
-		},
-		setToEl: (state, el) => {
-			state.toEl = el;
-		},
-		setScrollPosition: (state, el) => {
-			state.scrollPosition = el;
-		},
-		toggleIsTransitioning: state => {
-			state.isTransitioning = !state.isTransitioning;
-		},
-		setComponents: (state, components: string[]) => {
-			state.components = components;
-		},
-	},
-	actions: {},
+  state: {
+    counter: 2,
+    check: registry,
+    isSingleComponentRoute: false,
+    fromEl: null,
+    toEl: null,
+    isTransitioning: false,
+    scrollPosition: 0,
+    components: registry.components,
+    componentsHash: registry.componentsHash,
+  },
+  getters: {
+    getFiles: (state: any, getters) => () => {
+      return state.componentsHash;
+    },
+    getComponent: (state: any, getters) => (name: string) => {
+      for (const c of state.components) {
+        if (!c.default.options) {
+          continue;
+        }
+        if (c.default.options.name === name) {
+          return c;
+        }
+      }
+      return null;
+    },
+    getHtmlSingleFile: (state: any, getters) => (name: string) => {
+      const result = state.check.singleFileHash.get(name);
+      return result;
+    },
+    getScrollPosition: (state, getters) => () => {
+      return state.scrollPosition;
+    },
+    getComponents: (state, getters) => () => {
+      return state.components;
+    },
+  },
+  mutations: {
+    setFromEl: (state, el) => {
+      console.log(el, 'SetFromEl store.ts');
+      state.fromEl = el;
+    },
+    setToEl: (state, el) => {
+      state.toEl = el;
+    },
+    setScrollPosition: (state, el) => {
+      state.scrollPosition = el;
+    },
+    toggleIsTransitioning: state => {
+      state.isTransitioning = !state.isTransitioning;
+    },
+    setComponents: (state, components: string[]) => {
+      state.components = components;
+    },
+  },
+  actions: {},
 });
--- src/store/registry.ts
+++ src/store/registry.ts
@@ -1,12 +1,11 @@
 import { debug } from 'util';
 
-
 const ctx = require.context('../../src/', true, /.*.vue$/);
 const ctxHtml = require.context('!!raw-loader!../../src/', true, /.*.html$/);
 const ctxraw = require.context('!!raw-loader!../../src/', true, /.*.vue$/);
 
 interface Simple {
-	source: string;
+  source: string;
 }
 const components: any = ctx.keys().map(ctx);
 const componentsSource = ctxraw.keys().map(ctxraw);
@@ -16,81 +15,82 @@ const files = require.context(`../components/StyleGuide/`, true, /\.vue$/);
 
 const htmlFiles = require.context(`../mixifier/`, true, /\.html$/);
 for (let i = 0; i < components.length; i++) {
-	components[i].source = componentsSource[i];
+  components[i].source = componentsSource[i];
 }
-const singleFileHash = new Map<string, string>();
+const componentsHtmlHash = new Map<string, string>();
 
 htmlFiles.keys().forEach((key: string, index: number) => {
-	const path = key.replace(/(\.\/|\.html)/g, '');
-	const file = path.substr(path.indexOf('/') + 1, path.length);
+  const path = key.replace(/(\.\/|\.html)/g, '');
+  const file = path.substr(path.indexOf('/') + 1, path.length);
 
-	singleFileHash.set(file, componentsHtml[index]);
+  componentsHtmlHash.set(file, componentsHtml[index]);
 });
 
 function get(name: string) {
-	for (const c of components) {
-		if (!c.default.options) {
-			continue;
-		}
-		if (c.default.options.name === name) {
-			return c;
-		}
-	}
-	return null;
+  for (const c of components) {
+    if (!c.default.options) {
+      continue;
+    }
+    if (c.default.options.name === name) {
+      return c;
+    }
+  }
+  return null;
 }
 
 function getHtmlSingleFile(name: string) {
-	for (const c of componentsHtml) {
-		if (c.name === name) {
-			return c;
-		}
-	}
+  for (const c of componentsHtml) {
+    if (c.name === name) {
+      return c;
+    }
+  }
 }
 
 function buildHtmlSingleFiles() {
-	const hash = new Map<string, string[]>();
-	// Loading file names from a folder
-	htmlFiles.keys().forEach((key: string, index: number) => {
-		const path = key.replace(/(\.\/|\.html)/g, '');
-		const folder = path.substr(0, path.indexOf('/'));
-		const file = path.substr(path.indexOf('/') + 1, path.length);
+  const hash = new Map<string, string[]>();
+  // Loading file names from a folder
+  htmlFiles.keys().forEach((key: string, index: number) => {
+    const path = key.replace(/(\.\/|\.html)/g, '');
+    const folder = path.substr(0, path.indexOf('/'));
+    const file = path.substr(path.indexOf('/') + 1, path.length);
 
-		const currentFolder = hash.get(folder);
-		if (!currentFolder) {
-			hash.set(folder, new Array<string>());
-		}
-		currentFolder!.push(file);
-	});
-	return hash;
+    const currentFolder = hash.get(folder);
+    if (!currentFolder) {
+      hash.set(folder, new Array<string>());
+    }
+    currentFolder!.push(file);
+  });
+  return hash;
 }
 
 function buildRegistry() {
-	const hash = new Map<string, string[]>();
-	// Loading file names from a folder
-	files.keys().forEach((key: string) => {
-		const path = key.replace(/(\.\/|\.vue)/g, '');
-		const folder = path.substr(0, path.indexOf('/'));
-		const file = path.substr(path.indexOf('/') + 1, path.length);
+  const hash = new Map<string, string[]>();
+  // Loading file names from a folder
+  files.keys().forEach((key: string) => {
+    const path = key.replace(/(\.\/|\.vue)/g, '');
+    const folder = path.substr(0, path.indexOf('/'));
+    const file = path.substr(path.indexOf('/') + 1, path.length);
 
-		let currentFolder = hash.get(folder);
-		if (!currentFolder) {
-			hash.set(folder, new Array<string>());
-			currentFolder = hash.get(folder);
-		}
+    let currentFolder = hash.get(folder);
+    if (!currentFolder) {
+      hash.set(folder, new Array<string>());
+      currentFolder = hash.get(folder);
+    }
 
-		currentFolder!.push(file);
-	});
-	return hash;
+    currentFolder!.push(file);
+  });
+  return hash;
 }
 
-const componentHash = buildRegistry();
+const componentsHash = buildRegistry();
+console.log('Component Hash', componentsHash);
 
 export default {
-	components,
-	componentsHtml,
-	singleFileHash,
-	get,
-	buildRegistry,
-	buildHtmlSingleFiles,
-	componentHash,
+  components,
+  componentsHtml,
+  componentsHtmlHash,
+  get,
+  buildRegistry,
+  buildHtmlSingleFiles,
+  componentsHash,
 };
