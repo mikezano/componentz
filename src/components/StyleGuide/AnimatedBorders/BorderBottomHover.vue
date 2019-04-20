32c043edbd4b92a1647de422e30c65fc4723aefe
mikezano <mikezano@gmail.com>
2019-04-19 21:48:20 -0700

    new mixified

--- src/components/StyleGuide/AnimatedBorders/BorderBottomHover.vue
+++ src/components/StyleGuide/AnimatedBorders/BorderBottomHover.vue
@@ -1,45 +1,39 @@
 <template lang="pug">
-div.flex
-	.my-example.bottom-border-hover Hover Me
-	.my-example2.bottom-border-hover Hover Me 2
+.my-example.bottom-border-hover Hover Me
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-@Component({ name: "BorderBottomHover" })
+import { Component, Vue } from 'vue-property-decorator';
+@Component({ name: 'BorderBottomHover' })
 export default class BorderBottomHover extends Vue {}
 </script>
 
 
 <style lang="scss" scoped>
-@import "../../../sass/colors";
+@import '../../../sass/colors';
 
-
-@mixin bottom-border-hover($color : orange, $underline-size: 4px) {
+@mixin bottom-border-hover($color: orange, $underline-size: 4px) {
 	$s: bottom-border-hover;
 	&.#{$s} {
-		display:inline-block;
 		position: relative;
-		
-		&::before{
-			content:'';
-			position:absolute;
-			transition: width 0.5s ease-in-out;
-			width:0;
-			height:100%;
+		border-bottom: $underline-size solid white;
+		margin: 0 1rem;
+
+		&::before {
+			content: '';
+			position: absolute;
+			transition: width 0.25s ease-in-out;
+			width: 0;
+			height: 100%;
 		}
-		&:hover::before{
-			cursor:pointer;
+		&:hover::before {
+			cursor: pointer;
 			border-bottom: $underline-size solid $color;
-			width:100%;
+			width: 100%;
 		}
 	}
-}
-.my-example{
+} // bottom-border-hover end
+.my-example {
 	@include bottom-border-hover();
-}
-
-.my-example2{
-	@include bottom-border-hover(blue, 8px);
 }
 </style>
---
+++ src/components_examples/BorderBottomHoverExamples.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.examples
+	.example1.bottom-border-hover Wow!
+	.example2.bottom-border-hover Another Hover
+	.example3.bottom-border-hover One more!
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+@Component({ name: 'BorderBottomHoverExamples' })
+export default class BorderBottomHoverExamples extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import 'src/mixifier/ads_mixer.scss';
+@import '../sass/colors';
+
+.example1 {
+	@include bottom-border-hover(map-get($colors, 'z-blue'), 2px);
+}
+.example2 {
+	@include bottom-border-hover(map-get($colors, 'z-green'), 10px);
+}
+.example3 {
+	@include bottom-border-hover(map-get($colors, 'z-red'), 1px);
+}
+.examples {
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+}
+</style>
